db3899f57a0a47089b6b3ab22cb1778450efb487
llf200457 <2727917375@qq.com>
2024-05-27 12:20:36 +0800

    feat: 添加应用管理模块

---
+++ alinesno-infra-base-notice-domain/src/main/java/com/alinesno/infra/base/notice/mapper/ManagerApplicationMapper.java
@@ -0,0 +1,16 @@
+package com.alinesno.infra.base.notice.mapper;
+
+import com.alinesno.infra.base.notice.entity.ManagerApplicationEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+/**
+ * <p>
+ * 持久层接口
+ * </p>
+ *
+ * @author WeiXiaoJin
+ * @version 1.0.0
+ */
+public interface ManagerApplicationMapper extends IBaseMapper<ManagerApplicationEntity> {
+ 
+}
